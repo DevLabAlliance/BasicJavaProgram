6cde7e982b3c8fea8e90eeb82cad291c221ec753
user <user@prakharomar.local>
2020-04-28 21:14:23 +0530

    Calculator, new mehtod in linked list

---
+++ src/com/dla/Calculator.java
@@ -0,0 +1,92 @@
+package com.dla;
+
+import java.util.*;
+
+/**
+ * @author Devlabs Alliance
+ *
+ */
+
+// program to make calculator using Switch case
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.print("---------MENU----------\n1 :- Addition\n2 :- Subtraction\n3 :- Multiplication\n4 :-Division");
+		Scanner s = new Scanner(System.in);
+		int dla = s.nextInt();
+		while (dla != 5) {
+			switch (dla) {
+			case 1: {
+
+				System.out.println("Enter two number to add");
+				int a = s.nextInt();
+				int b = s.nextInt();
+				System.out.printf("Result of addition is %d\n\n\n", a + b);
+				System.out.println(
+						"---------MENU----------\n1 :- Addition\n2 :- Subtraction\n3 :- Multiplication\n4 :-Division");
+				dla = s.nextInt();
+				break;
+			}
+
+			case 2: {
+
+				System.out.println("Enter two number to subtract");
+				int a = s.nextInt();
+				int b = s.nextInt();
+				System.out.printf("Result of subtraction is %d\n\n\n", a - b);
+				System.out.println(
+						"---------MENU----------\n1 :- Addition\n2 :- Subtraction\n3 :- Multiplication\n4 :-Division");
+				dla = s.nextInt();
+				break;
+			}
+			case 3: {
+
+				System.out.println("Enter two number to multiply");
+				int a = s.nextInt();
+				int b = s.nextInt();
+				System.out.printf("Result of multiplication is %d", a * b);
+				System.out.println(
+						"---------MENU----------\n1 :- Addition\n2 :- Subtraction\n3 :- Multiplication\n4 :-Division");
+				dla = s.nextInt();
+				break;
+			}
+			case 4: {
+
+				System.out.println("Enter two number to divide");
+				int a = s.nextInt();
+				int b = s.nextInt();
+
+				while (b == 0) {
+					System.out.println("Division is not by 0");
+					System.out.println("Enter the numbers again");
+					a = s.nextInt();
+					b = s.nextInt();
+					
+				}				
+				if(b!=0)
+				{
+					System.out.printf("Result of divison is %d\n", a / b);
+				System.out.println(
+						"---------MENU----------\n1 :- Addition\n2 :- Subtraction\n3 :- Multiplication\n4 :-Division");
+				dla = s.nextInt();
+				break;
+			  
+			   }
+			}
+			
+			case 5: {
+				System.out.println("Exit");
+				break;
+			}
+			default:
+				System.out.println("Unexpected value: " + dla);
+				System.out.println("Enter value between 1 and 5\n\n\n\n");
+				System.out.println(
+						"---------MENU----------\n1 :- Addition\n2 :- Subtraction\n3 :- Multiplication\n4 :-Division");
+				dla = s.nextInt();
+				break;
+			}
+		}
+	}
+}
--- src/com/dla/SinglyLinkedList.java
+++ src/com/dla/SinglyLinkedList.java
@@ -9,17 +9,6 @@ package com.dla;
 
 public class SinglyLinkedList {
 
-	class Node {
-
-		int data;
-		Node next;
-
-		public Node(int data) {
-			this.data = data;
-			this.next = null;
-		}
-	}
-
 	public Node head = null;
 	public Node tail = null;
 
@@ -29,54 +18,132 @@ public class SinglyLinkedList {
 		Node newNode = new Node(data);
 
 		if (head == null) {
-			
+
 			// if list is empty, both head and tail point to new node
 			head = newNode;
 			tail = newNode;
 		}
 
 		else {
-			
+
 			// new node will created and tail next will point to this
 			tail.next = newNode;
-			
+
 			// last node will became the tail
 			tail = newNode;
 
 		}
 	}
-	
+
 	public void display() {
-		
-		// point to head		
+
+		// point to head
 		Node current = head;
-		
-		if(head==null) {
+
+		if (head == null) {
 			System.out.println("List is empty");
 			return;
 		}
-		
+
 		// print while it reaces the last node
-		while(current!=null) {
-			System.out.println(current.data+" ");
-			
+		while (current != null) {
+			System.out.println(current.data + " ");
+
 			// pointing to next node
 			current = current.next;
 		}
 	}
 
+	/**
+	 * @param key is the data from the list we want to delete
+	 */
+	public void delete(int key) {
+
+		Node current = head;
+		Node temp = head;
+
+		if (head.data == key) {
+			head = current.next;
+		}
+
+		while (current.data != key) {
+			temp = current;
+			current = current.next;
+		}
+
+		temp.next = current.next;
+		current.next = null;
+
+	}
+
+	/**
+	 * @param key is the data we want to add
+	 */
+
+	/* method to add node at the end of the list */
+	public void addAtEnd(int key) {
+
+		Node current = head;
+		while (current.next != null) {
+			current = current.next;
+		}
+
+		Node newnode = new Node(key);
+		current.next = newnode;
+		tail = newnode;
+	}
+
+	/**
+	 * @param key is the data we want to add
+	 */
+
+	/* method to add node at the start of the list */
+	public void addAtStart(int key) {
+
+		Node newnode = new Node(key);
+		if (head == null) {
+			System.out.println("lsit is empty");
+			head = newnode;
+			tail = newnode;
+		}
+
+		else {
+			newnode.next = head;
+			head = newnode;
+		}
+	}
+
 	public static void main(String[] args) {
-		
+
 		// object of list
 		SinglyLinkedList dlalist = new SinglyLinkedList();
-		
+
 		// adding node to list
 		dlalist.addNode(1);
-		dlalist.addNode(9);
-		dlalist.addNode(7);
 		dlalist.addNode(5);
-        
-        // display the node element		
+		dlalist.addNode(7);
+		dlalist.addNode(9);
+		dlalist.addNode(11);
+
+		dlalist.addAtStart(34);
+		dlalist.addAtStart(4);
+
+		dlalist.delete(7);
+		dlalist.addAtEnd(67);
 		dlalist.display();
+
 	}
+}
+
+// class to create node
+class Node {
+	int data;
+	Node next;
+
+	// constructor
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
 }
